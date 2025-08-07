8d51363e4c046d9902bdf71fea4509720ffdc7d8
임성윤 <seongyoonhere@gmail.com>
2025-08-07 13:57:46 +0900

    [gateway-service]: initial commit

--- discovery-service/src/main/java/com/sy/discovery_service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/sy/discovery_service/DiscoveryServiceApplication.java
@@ -1,8 +1,16 @@
 package com.sy.discovery_service;
 
+import com.netflix.appinfo.AmazonInfo;
+import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +20,17 @@ public class DiscoveryServiceApplication {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
+	/*
+	* To make eureka server application configured to be AWS-aware,
+	* customize the EurekaInstanceConfigBean
+	* */
+
+//	@Bean
+//	@Profile("!default")
+//	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+//		EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+//		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+//		b.setDataCenterInfo(info);
+//		return b;
+//	}
 }
